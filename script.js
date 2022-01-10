d9943844d6629a3695825a472e99723ba5b19806
Sherzad91 <93314728+Sherzad91@users.noreply.github.com>
2022-01-10 13:57:37 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+var a = 15;
+var b = 17;
+    console.log(a + b)
+
+var numberone = 20;
+var numbertwo = 10;
+var Numcorect = numberone > numbertwo;
+console.log(Numcorect);
+
+var numberone = 20;
+var numbertwo = 10;
+var Numcorect = numberone < numbertwo;
+console.log(Numcorect);
+
+var stringcar = "Audi";
+console.log(stringcar)
+
+
+var firstname = "Sherzad";
+var lastname = "Hasan";
+
+console.log(firstname);
+console.log(lastname);
+console.log(firstname,lastname);
+
+
+var animal = ("Frog");
+if ("true"){
+    console.log(animal,"is animal");
+}
+
+
+var order = "true";
+if (order ="shipped"){
+    console.log("The order shipped");
+
+    }
+    else {
+    console.log("The order did not shipped");
+}
+
+var nummer = "i";
+for (i = 7; i < 13; i++){
+    console.log(i)
+}
